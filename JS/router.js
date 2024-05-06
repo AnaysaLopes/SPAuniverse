f06f9f971fc0e1eaa171c7613dad88d511e7fcc0
Anaysa Lopes <153683976+AnaysaLopes@users.noreply.github.com>
2024-05-06 15:22:35 -0300

    Add files via upload

---
+++ JS/router.js
@@ -0,0 +1,39 @@
+export class Router {
+    routes = {} 
+  
+    add(routeName, page) { 
+      this.routes[routeName] = page 
+    }
+    
+    route(event) { 
+      event = event || window.event 
+      event.preventDefault() 
+  
+      window.history.pushState({}, "", event.target.href) 
+  
+      this.handle()
+    }
+  
+    handle() { 
+      const { pathname }  = window.location 
+      const route = this.routes[pathname] || this.routes[404]
+      fetch(route) 
+      .then(data => data.text()) 
+      .then(html => { 
+          document.querySelector('#app').innerHTML = html;
+          document.body.style.backgroundImage = this.getBackgroundImage(pathname);
+      })
+  }
+  
+  getBackgroundImage(pathname) {
+      switch(pathname) {
+          case '/universe':
+              return 'url("./assets/background2.jpg")';
+          case '/exploration':
+              return 'url("./assets/background3.jpg")'; 
+          default:
+              return 'url("./assets/background1.jpg")';
+      }
+  }
+  
+}  
